3554f7329447c84bc0f793b880dfc07fc329fd05
Michael Shen <mshen@redhat.com>
2022-07-28 17:10:46 -0400

    cleanup hiveutil, operator-sdk
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,8 +1,6 @@
 final: prev: {
-  hiveutil = prev.callPackage ./hiveutil.nix { };
   oc = prev.callPackage ./oc.nix { };
   ocm = prev.callPackage ./ocm.nix { };
-  operator-sdk = prev.callPackage ./operator-sdk.nix { };
   osdctl = prev.callPackage ./osdctl.nix { };
   pd = prev.callPackage ./go-pagerduty.nix { };
   rosa = prev.callPackage ./rosa.nix { };
