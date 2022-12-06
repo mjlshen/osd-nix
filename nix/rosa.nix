a5700198eb8ea37f4c003b08ed155008a8f3432e
Michael Shen <mshen@redhat.com>
2022-12-05 23:18:56 -0500

    rosa v1.2.10
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/rosa.nix
+++ nix/rosa.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "rosa";
-  version = "v1.2.8";
+  version = "v1.2.10";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "rosa";
     rev = "${version}";
-    sha256 = "sha256-4S6AWBRML9Sexbxg9p/lJQp3gqYAdcM/zYXG+Md5Lak=";
+    sha256 = "sha256-jdXM5w+9FfGEKC4MF9dbTDlng8agJM10qQtMDpom6zs=";
   };
 
   ldflags = [
