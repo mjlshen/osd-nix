6699fc581599b77a91deb6221d81f8b95c9baf9e
Michael Shen <mshen@redhat.com>
2022-12-19 21:17:35 -0500

    ocm v0.1.65
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/ocm.nix
+++ nix/ocm.nix
@@ -2,20 +2,20 @@
 
 buildGoModule rec {
   pname = "ocm";
-  version = "0.1.64";
+  version = "0.1.65";
 
   src = fetchFromGitHub {
     owner = "openshift-online";
     repo = "ocm-cli";
     rev = "v${version}";
-    sha256 = "sha256-RMXiEXgf8tAdp2d97kaOzXgFCFVkaMhkJF8AHXIEJm8=";
+    sha256 = "sha256-UzHGVK/HZ5eH8nO4+G92NunOQi9AWnqv4vgcHjtoPDw=";
   };
 
   ldflags = [
     "-s" "-w"
   ];
 
-  vendorSha256 = "sha256-4m5Ej2Ql9+wGqrzvXQkY8fL2I9tYE6Tm6s9+qcZBHQI=";
+  vendorSha256 = "sha256-4pqXap1WayqdXuwwLktE71D7x6Ao9MkIKSzIKtVyP84=";
 
   subPackages = [ "cmd/ocm" ];
 }
