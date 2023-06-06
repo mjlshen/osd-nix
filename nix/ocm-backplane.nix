5f868f1bed36c769b5c8a4e436070b95f86ea0c3
Michael Shen <mshen@redhat.com>
2023-06-06 08:24:42 -0400

    ocm-backplane v0.1.5
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/ocm-backplane.nix
+++ nix/ocm-backplane.nix
@@ -2,13 +2,13 @@
 
 buildGo118Module rec {
   pname = "backplane-cli";
-  version = "0.1.4";
+  version = "0.1.5";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "backplane-cli";
     rev = "v${version}";
-    sha256 = "sha256-nclB/SGRHEAhcCTRAI2YgORBhGtEJZG0UctfOGKcXg8=";
+    sha256 = "sha256-HfuR4uqCre19TBHSXGSOC40bLoMpnVanI8sXRohnabs=";
   };
 
   CGO_ENABLED = 0;
@@ -19,7 +19,7 @@ buildGo118Module rec {
     "-X github.com/openshift/backplane-cli/pkg/info.Version=${version}"
   ];
 
-  vendorSha256 = "sha256-j6uY5ficZ154PXzGSv7AaAc2TIH3V+qljzGdlueih9U=";
+  vendorSha256 = "sha256-OWfY9helfw5gIgA9ZhNLVXAsZ5tnVZ0flQLnWOe0a/s=";
 
   subPackages = [ "cmd/ocm-backplane" ];
 }
