f270479319e8cac2caaf73e9e9a29570d7f79597
Michael Shen <mshen@redhat.com>
2023-06-14 11:06:08 -0400

    osdctl v0.16.0
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/osdctl.nix
+++ nix/osdctl.nix
@@ -2,13 +2,13 @@
 
 buildGo118Module rec {
   pname = "osdctl";
-  version = "0.15.0";
+  version = "0.16.0";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "osdctl";
     rev = "v${version}";
-    sha256 = "sha256-TxL/a9Gis5IPVQ+SNUe++ZUmAHc+W/NTpRb7I4FE+YY=";
+    sha256 = "sha256-vulKgCmRbYOLHxRwXB6HcJMW1VXpGJzEEZmQttRpnAw=";
   };
 
   CGO_ENABLED = 0;
@@ -18,6 +18,6 @@ buildGo118Module rec {
     "-X github.com/openshift/osdctl/pkg/utils.Version=${version}"
   ];
 
-  vendorSha256 = "sha256-QsJfR6PR7LZGGN9xWIZun5kdvH7msTvHkkfybi3tfEw=";
+  vendorSha256 = "sha256-laBaOFEno+AfBraEm+tcPgZa8xeCoNxYDM7FSZz3iAM=";
   doCheck = false;
 }
