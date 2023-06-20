f132afc4288db8f5852f2c5175d99af69897363f
Michael Shen <mshen@redhat.com>
2023-06-20 10:14:10 -0400

    hypershift v4.13
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/hypershift.nix
+++ nix/hypershift.nix
@@ -2,13 +2,13 @@
 
 buildGo118Module rec {
   pname = "hypershift";
-  version = "4.12";
+  version = "4.13";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "hypershift";
     rev = "release-${version}";
-    sha256 = "sha256-c8oWcX98T8+owFNhK1ccyRZ44HFAUyUclZqPIyYNjBQ=";
+    sha256 = "sha256-MeQ1vNi0obWeEagPQxc4Lris4NGSVEoN6X1hukSV0kk=";
   };
 
   vendorSha256 = null;
