22fe0b046eaad89751cdf7ce6fb69f5d8ce4ba57
Michael Shen <mshen@redhat.com>
2023-08-29 23:06:06 -0400

    update to nixpkgs-unstable for buildGo121Module
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- nix/mirrosa.nix
+++ nix/mirrosa.nix
@@ -1,14 +1,14 @@
-{ buildGo120Module, fetchFromGitHub }:
+{ buildGo121Module, fetchFromGitHub }:
 
-buildGo120Module rec {
+buildGo121Module rec {
   pname = "mirrosa";
-  version = "0.0.5";
+  version = "0.0.6";
 
   src = fetchFromGitHub {
     owner = "mjlshen";
     repo = "mirrosa";
     rev = "v${version}";
-    sha256 = "sha256-xFAMaERh08n4899Xkt/yide/3V5dcbF/4ere8sVKlis=";
+    sha256 = "sha256-tV1rta2os4iP+Yl5weLcX5NOaale7/n7Ea9mckYg148=";
   };
 
   CGO_ENABLED = 0;
@@ -17,5 +17,5 @@ buildGo120Module rec {
     "-s" "-w"
   ];
 
-  vendorSha256 = "sha256-J5qkqtOUme8agEJ7K70/sdD364r9fWcR0CL6DsjcM9o=";
+  vendorSha256 = "sha256-ztjUCFPQZv1Juc2INAYoXsl5eb8boyPtQNm4Mh4Ll4Q=";
 }
