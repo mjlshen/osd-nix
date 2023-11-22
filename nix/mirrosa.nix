c78560b55ca3e5cc937b5811c25c452626c5291e
Michael Shen <mishen@umich.edu>
2023-11-21 21:36:14 -0500

    mirrosa v0.0.7
    Signed-off-by: Michael Shen <mishen@umich.edu>

--- nix/mirrosa.nix
+++ nix/mirrosa.nix
@@ -2,13 +2,13 @@
 
 buildGo121Module rec {
   pname = "mirrosa";
-  version = "0.0.6";
+  version = "0.0.7";
 
   src = fetchFromGitHub {
     owner = "mjlshen";
     repo = "mirrosa";
     rev = "v${version}";
-    sha256 = "sha256-tV1rta2os4iP+Yl5weLcX5NOaale7/n7Ea9mckYg148=";
+    sha256 = "sha256-KzxbnjJobUXF1oBGL4GTW8ErKCXiUR/IJSlAA554ops=";
   };
 
   CGO_ENABLED = 0;
@@ -17,5 +17,5 @@ buildGo121Module rec {
     "-s" "-w"
   ];
 
-  vendorSha256 = "sha256-ztjUCFPQZv1Juc2INAYoXsl5eb8boyPtQNm4Mh4Ll4Q=";
+  vendorSha256 = "sha256-IHprVBKYo1Q2zFj82AsDJaRS1VwRYXqJg3+ierwM+1o=";
 }
