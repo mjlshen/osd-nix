c9bf782a99d8130cec0535eb02957f2afeaa51ea
Michael Shen <mishen@umich.edu>
2023-11-30 16:05:21 -0500

    rosa v1.2.31
    Signed-off-by: Michael Shen <mishen@umich.edu>

--- nix/rosa.nix
+++ nix/rosa.nix
@@ -2,13 +2,13 @@
 
 buildGo120Module rec {
   pname = "rosa";
-  version = "v1.2.30";
+  version = "v1.2.31";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "rosa";
     rev = "${version}";
-    sha256 = "sha256-Uu+LmGiejhoktWh9v2kzZId2UbOWQmbfrP0LWx3yHr0=";
+    sha256 = "sha256-DDoou/3CTn+WxrACudrX0YIyfZbhbOfMOH5XC1ud4vY=";
   };
 
   CGO_ENABLED = 0;
