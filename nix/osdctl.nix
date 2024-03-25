074eff9e66840e1f067c1816da75df6876b8d998
Michael Shen <mishen@umich.edu>
2024-03-25 09:22:11 -0400

    osdctl v0.28.0
    Signed-off-by: Michael Shen <mishen@umich.edu>

--- nix/osdctl.nix
+++ nix/osdctl.nix
@@ -2,13 +2,13 @@
 
 buildGo121Module rec {
   pname = "osdctl";
-  version = "0.27.1";
+  version = "0.28.0";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "osdctl";
     rev = "v${version}";
-    sha256 = "sha256-aIDaiT51ceELEqQda16+brRYS3XM9mJDdaIaTPGDiLo=";
+    sha256 = "sha256-+23OkWlPlGZZPjuhBSp1Id1H+Ep67DciFmTSlD8bbCo=";
   };
 
   CGO_ENABLED = 0;
@@ -18,6 +18,6 @@ buildGo121Module rec {
     "-X github.com/openshift/osdctl/pkg/utils.Version=${version}"
   ];
 
-  vendorSha256 = "sha256-Lc243e3HrxvO4QlfchZsomAPkF+TaN5SidyAgm/EvtA=";
+  vendorSha256 = "sha256-Lzdp422N6pNXzOh2hNpQSkB8m2kSHHL1eKl3iAhWHig=";
   doCheck = false;
 }
