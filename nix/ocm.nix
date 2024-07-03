f672f3a5ab4f78d9720425b7d4543f6688e9b398
Michael Shen <mishen@umich.edu>
2024-07-03 16:06:31 -0400

    ocm v0.1.74
    Signed-off-by: Michael Shen <mishen@umich.edu>

--- nix/ocm.nix
+++ nix/ocm.nix
@@ -2,20 +2,20 @@
 
 buildGo118Module rec {
   pname = "ocm";
-  version = "0.1.73";
+  version = "0.1.74";
 
   src = fetchFromGitHub {
     owner = "openshift-online";
     repo = "ocm-cli";
     rev = "v${version}";
-    sha256 = "sha256-hcbCUzC+E7VHmqATw/1LQxdMLiFibgYiWrQHpm3jZLA=";
+    sha256 = "sha256-sORCttAeLmC1Ab4LmdQaYHA9nWDx+pN6rcts+xVHXFY=";
   };
 
   ldflags = [
     "-s" "-w"
   ];
 
-  vendorSha256 = "sha256-NP5LLP27hn8p2gUVO/qpdu1Yp3t8iarUUKR/FjU0Qlc=";
+  vendorSha256 = "sha256-hYNSOzXgXwNTwJywPyFktDp5NA1ROKZtSjMsDdz7nSg=";
 
   subPackages = [ "cmd/ocm" ];
 }
