81f8649e9ad743088831a4265478913ce0647a81
Michael Shen <mishen@umich.edu>
2024-07-08 10:20:56 -0400

    ocm-backplane v0.1.33
    Signed-off-by: Michael Shen <mishen@umich.edu>

--- nix/ocm-backplane.nix
+++ nix/ocm-backplane.nix
@@ -2,13 +2,13 @@
 
 buildGo121Module rec {
   pname = "backplane-cli";
-  version = "0.1.32";
+  version = "0.1.33";
 
   src = fetchFromGitHub {
     owner = "openshift";
     repo = "backplane-cli";
     rev = "v${version}";
-    sha256 = "sha256-ex4QZKxjAx1gnaUYyE1vbYHIfM9/YxNKzjC0Og2ms1U=";
+    sha256 = "sha256-FUvEVy/r4gZ7h47iT/E9kM/C9WxIXz/D03CSYmkVkr0=";
   };
 
   CGO_ENABLED = 0;
@@ -19,7 +19,7 @@ buildGo121Module rec {
     "-X github.com/openshift/backplane-cli/pkg/info.Version=${version}"
   ];
 
-  vendorSha256 = "sha256-Gof1mcVZkmm1yNIx8Gw7pYM9pYZUWQzxSRa4m9SdWww=";
+  vendorSha256 = "sha256-RYmN5+yheSnXZvIQGNbbwb2rsK6gqSD6kbcE6F3b5JQ=";
 
   subPackages = [ "cmd/ocm-backplane" ];
 }
